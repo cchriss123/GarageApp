d19691574320e859a561ee88617e2a09dd341791
Christofer Nyström <nystromchristofer@gmail.com>
2026-05-11 12:24:59 +0200

    Adds Press any key to continue function

---
+++ GarageApp/Ui/UiHelper.cs
@@ -0,0 +1,10 @@
+namespace GarageApp.Ui;
+
+public class UiHelper
+{
+        private static void PressEnterToContinue()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+}
